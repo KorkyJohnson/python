cb53ef1ed11f70b0e644414c52487df28796cded
Korky Johnson <korkyjohnson@gmail.com>
2025-04-13 20:15:45 +0000

    finished ass4

--- 4_processnum/processNum.py
+++ 4_processnum/processNum.py
@@ -4,9 +4,18 @@ def main():
 
     with open("numbers.txt") as numberRead:
         for num in numberRead:
-            numberList.append(num.rstrip())
+            try:
+                num = float(num.rstrip())
+                numberList.append(num)
+            except ValueError:
+                print(f'value "{num.rstrip()}" is not a valid number. Skipping')
 
     print(f'The numbers are:  {numberList}')
+    print("The max number is ", max(numberList))
+    print("The min number is ", min(numberList))
+    print("The sum of numbers is ", sum(numberList))
+    print("The count of numbers is ", len(numberList))
+    print("The avg number is ", round(sum(numberList) / len(numberList),2))
 
 if __name__ == "__main__":
     main()
